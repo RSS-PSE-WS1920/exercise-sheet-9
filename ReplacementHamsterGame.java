ba1163a92b2e6f59405fcd92c313a747802fe0cd
mfrank <markus.frank@informatik.tu-chemnitz.de>
2018-12-14 09:50:40 +0100

    add files for part B and territors

---
+++ ReplacementHamsterGame.java
@@ -0,0 +1,30 @@
+import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.Hamster;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Location;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.Territory;
+import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Direction;
+import java.io.IOException;
+
+/**
+ * Write a description of class ReplacementHamsterGame here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ReplacementHamsterGame extends SimpleHamsterGame {
+
+	public ReplacementHamsterGame() {
+		game.displayInNewGameWindow();
+		try {
+			game.initialize("/territories/rotate.ter");
+
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	public void rotate180() {
+	}
+
+	public void rotate90() {
+	}
+}
